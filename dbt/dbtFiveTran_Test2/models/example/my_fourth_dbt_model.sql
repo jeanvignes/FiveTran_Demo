57d0f65d6da6d36ca4a9c2adccf322ed2fd60d93
jeanvignes <144738664+jeanvignes@users.noreply.github.com>
2024-12-06 10:56:29 -0600

    Update my_fourth_dbt_model.sql

--- dbt/dbtFiveTran_Test2/models/example/my_fourth_dbt_model.sql
+++ dbt/dbtFiveTran_Test2/models/example/my_fourth_dbt_model.sql
@@ -3,7 +3,7 @@
 
 
 select *
-from {{ ref('my_third_dbt_model') }};
+from {{ ref('Series_T1_001_calendar_subset_w_pretty_names') }};
 
-select *
-from {{ ref('Change_CALENDAR_SUBSET6_dbt_model') }}
+--select *
+--from {{ ref('Change_CALENDAR_SUBSET6_dbt_model') }}
